30c9d2d844001cae5d89d929660f671df7722e9b
Wafiy HM <wafiy@anak.it>
2024-08-26 23:20:37 +0800

    declare constant

--- resources/config.php
+++ resources/config.php
@@ -1,24 +1,22 @@
 <?php
 
+ob_start();
+
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 
+//declare CSS template constant
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
-
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
 
+//declare DB constant
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASS") ? null : define("DB_PASS", "pass123");
+defined("DB_NAME") ? null : define("DB_NAME", "ecom_db");
 
+//establish connection
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-
-
-
-
-
-
-
-
-
-
-
-
+require_once("functions.php");
 
 ?>
